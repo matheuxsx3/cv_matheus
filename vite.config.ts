98d09d42452cf5f50bcebdd463fb85498d3b0781
Matheus Pereira da Silva <131720752+matheuxsx3@users.noreply.github.com>
2024-09-21 22:04:55 -0300

    Refactor deployment configuration and build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +10,8 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-})
+  base: "/cv_matheus/",
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+});
